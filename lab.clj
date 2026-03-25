099b9efe97d697ed881919748900a04946024c93
riteemo <irina.qqu@gmail.com>
2026-03-26 01:52:47 +0300

    Тестовый список пользователей

---
+++ lab.clj
@@ -0,0 +1,9 @@
+(def users
+  [{:name "Anna"  :age 20 :city "Moscow"}
+   {:name "Boris" :age 21 :city "Kazan"}
+   {:name "Vera"  :age 20 :city "Moscow"}
+   {:name "Gleb"  :age 22 :city "Sochi"}
+   {:name "Dasha" :age 21 :city "Kazan"}
+   {:name "Egor"  :age 20 :city "Sochi"}])
+
+(println users)
